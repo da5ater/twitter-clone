e2c80caa222673d1be6517fe61fe0e5082ffff6b
mohamed <mohamedsocial555@gmail.com>
2025-10-01 02:41:33 +0300

    Refactor login page layout and styles
    - Updated login route to use 'main-layout' instead of 'login-layout'.
    - Removed deprecated 'login-layout.hbs' file.
    - Modified 'main-layout.hbs' to streamline structure and ensure proper styling.
    - Refactored 'login.hbs' to enhance layout and improve readability.
    - Adjusted footer partial for better alignment and consistency.
    - Commented out media query for grid layout in 'login.scss' for future use.

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 // profile route
 router.get('/login', (req, res) => {
-  res.render('login', { title: 'Login', layout: null });
+  res.render('login', { title: 'Login', layout: 'main-layout' });
 });
 
 router.get('/register', (req, res) => {
-  res.render('register', { title: 'Register' });
+  res.render('register', { title: 'Register', layout: 'main-layout' });
 });
 
 module.exports = router;
